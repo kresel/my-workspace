3140e8005abba79047cf411588db079255a8099e
kresel <Kresel115@gmail.com>
2017-02-15 22:07:28 -0700

    dsd

--- knife.rb
+++ knife.rb
@@ -1,8 +1,8 @@
 log_level                :info
 log_location             STDOUT
-node_name                'fb'
-client_key               'C:/Users/Administrator/.chef/fb.pem'
+node_name                'souschef'
+client_key               'C:/Users/Administrator/.chef/souschef.pem'
 validation_client_name   'fishbowl-validator'
-validation_key           'C:/etc/chef-server/chef-validator.pem'
-chef_server_url          'https://ChefServer.fishbowl.com:443/organizations/fishbowl'
+validation_key           'C:/etc/chef-server/fishvowl-validator.pem'
+chef_server_url          'https://IronChef.fishbowl.com:443/organizations/fishbowl'
 syntax_check_cache_path  'C:/Users/Administrator/.chef/syntax_check_cache'
