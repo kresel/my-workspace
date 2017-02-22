1bee4b89219d70b3b25e86c08135d026c074ef70
kresel <Kresel115@gmail.com>
2017-02-22 13:39:29 -0700

    poppy

--- Install_RDS_Web_Broker_Sesion_host.ps1
+++ Install_RDS_Web_Broker_Sesion_host.ps1
@@ -1,3 +1,12 @@
 #this part has to be ran off of the machine that it is going to be installing it too 
 Import-Module RemoteDesktop
-New-SessionDeployment -ConnectionBroker KK_testBox.fishbowl.com -WebAccessServer KK_testBox.fishbowl.com -SessionHost KK_testBox.fishbowl.com 
+$theFQDN = "KK_testbox.fishbowl.com"
+$theCollectionName = "Dubuque Coffee"
+#Creates the RDS system with connection broker, Web access server, and the session host
+New-SessionDeployment -ConnectionBroker $theFQDN -WebAccessServer $theFQDN -SessionHost $theFQDN 
+
+#The code creates and names the collection and gives it a discription However it does not give exact users right to the collection it is open to all the users 
+New-RDSessionCollection -CollectionName $theCollectionName -SessionHost $theFQDN -CollectionDescription "Customers collection of programs" -ConnectionBroker $theFQDN
+
+# This code right here is to add porgram to the collection HOWEVER I dont know were the atom program is on the collections secsion 
+#New-RDRemoteApp -Alias Wordpad - Wordpad -FilePath  "C:\Users\Administrator\AppData\Local\atom\app-1.14.3\atom.exe"-ShowInWebAccess 1 -CollectionName $theCollectionName -ConnectionBroker $theFQDN
---
+++ Setting_RDS_Collections.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module RemoteDesktop
+$theFQDN = "KK_testbox.fishbowl.com"
+$theCollectionName = "Dubuque Coffee"
+New-RDRemoteApp -Alias Atom -DisplayName Atom -FilePath  "C:\Users\Administrator\AppData\Local\atom\app-1.14.3\atom.exe"-ShowInWebAccess 1 -CollectionName $theCollectionName -ConnectionBroker $theFQDN
